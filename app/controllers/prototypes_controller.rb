b99fb0a2dbb44f9d1117288082d9b6d56334e1e3
fumiya <f40ya246@icloud.com>
2025-06-25 21:58:32 +0900

    Active Storage 導入(修正)

--- app/controllers/prototypes_controller.rb
+++ app/controllers/prototypes_controller.rb
@@ -1,4 +1,26 @@
 class PrototypesController < ApplicationController
   def index
+    @prototypes = Prototype.all
+  end
+
+  def new
+    @prototype = Prototype.new
+  end
+
+  def create
+    @prototype = Prototype.new(prototype_params)
+    @prototype.user = current_user
+
+    if @prototype.save
+      redirect_to root_path
+    else
+      render :new, status: :unprocessable_entity
+    end
+  end
+
+  private
+
+  def prototype_params
+    params.require(:prototype).permit(:title, :catch_copy, :concept, :image)
   end
 end
